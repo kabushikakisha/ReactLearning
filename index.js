8d540864ab3a27c22acec1c00d6e12a031ee3578
kabushikakisha <37305991+kabushikakisha@users.noreply.github.com>
2018-05-28 16:22:39 -0500

    finished for afternoon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const
+const style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'verdana'
+}
+
+ReactDOM.render(
+    <h1 id='title' className='header' style={style}>Hello World</h1>,
+    document.getElementById('react-container')
+);
